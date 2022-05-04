86678f4d82a4cddc3fce4ebc7ebbed18f490ec97
GabrielZaffalon <gabrielzaffalon19@gmail.com>
2022-05-04 18:00:46 -0300

    feat(card): adding card component

--- reactnativestudies/src/routes/app-stack.js
+++ reactnativestudies/src/routes/app-stack.js
@@ -2,17 +2,23 @@ import React from 'react'
 import { View } from 'react-native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-import Button from '../components/Button'
+import Button from '../components/button'
+import Card from '../components/card'
 
 const Stack = createNativeStackNavigator()
 
 const Teste = () => (
-  <View style={{ flex: 1, backgroundColor: 'red', alignItems: 'center', justifyContent: 'center' }}>
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
     <Button
       text='botão'
       onPress={() => {
         console.log('apertou')
       }}
+    />
+    <Card
+      title='Esse é o título'
+      description='Essa é a descrição'
+      onNavigateToApp={() => console.log('Apertou Card')}
     />
   </View>
 )
