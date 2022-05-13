d065149ae3a6f4921f1c529005a022b7b6dd22a9
GabrielZaffalon <gabrielzaffalon19@gmail.com>
2022-05-13 16:42:02 -0300

    fix: integrate the use of FlatList

--- reactnativestudies/src/screens/list.js
+++ reactnativestudies/src/screens/list.js
@@ -1,15 +1,38 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, FlatList, StyleSheet } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 
 import Card from '../components/card'
+
+const SCREENS = [
+  { title: 'Relógio', description: 'Mostra as horas', navigate: 'Relógio' },
+  {
+    title: 'Clima',
+    description: 'Mostra as informações climáticas da sua região',
+    navigate: 'Clima'
+  }
+]
 
 const List = () => {
   const navigation = useNavigation()
 
   return (
     <View style={styles.container}>
-      <Card
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ flex: 1 }}
+        data={SCREENS}
+        renderItem={({ item }) => {
+          return (
+            <Card
+              title={item.title}
+              description={item.description}
+              onNavigateToApp={() => navigation.navigate(item.navigate)}
+            />
+          )
+        }}
+      />
+      {/* <Card
         title='Relógio'
         description='Mostra as horas'
         onNavigateToApp={() => navigation.navigate('Relógio')}
@@ -18,7 +41,7 @@ const List = () => {
         title='Clima'
         description='Mostra as informações climáticas da sua região'
         onNavigateToApp={() => navigation.navigate('Clima')}
-      />
+      /> */}
     </View>
   )
 }
@@ -28,8 +51,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 20,
-    marginVertical: 20
+    padding: 20
   }
 })
 
