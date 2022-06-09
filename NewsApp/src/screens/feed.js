faa5e5bb95d2072ce4d51f815ea88a520d4874e8
GabrielZaffalon <gabrielzaffalon19@gmail.com>
2022-06-09 16:17:59 -0300

    feat (component): button

--- NewsApp/src/screens/feed.js
+++ NewsApp/src/screens/feed.js
@@ -15,6 +15,7 @@ import ColumnCards from '../components/ColumnImagesCards'
 import { showHeadLines } from '../services/news'
 import LoadingRowCard from '../components/LoadingRowCard'
 import LoadingColumCard from '../components/LoadingColumCard'
+import Button from '../components/button'
 
 const Feed = () => {
   const navigation = useNavigation()
@@ -76,25 +77,30 @@ const Feed = () => {
         {loading ? (
           <LoadingColumCard />
         ) : (
-          <FlatList
-            showsVeticalScrollIndicator={false}
-            scrollEnabled={false}
-            data={feed}
-            keyExtractor={item => item.url}
-            contentContainerStyle={{ paddingLeft: 22 }}
-            renderItem={({ item }) => {
-              return (
-                <TouchableOpacity onPress={() => navigation.navigate('News', { news: item })}>
-                  <ColumnCards
-                    image={item.urlToImage}
-                    author={item.author != null ? item.author : 'Unkown'}
-                    title={item.title}
-                    date={item.publishedAt}
-                  />
-                </TouchableOpacity>
-              )
-            }}
-          />
+          <View>
+            <FlatList
+              showsVeticalScrollIndicator={false}
+              scrollEnabled={false}
+              data={feed}
+              keyExtractor={item => item.url}
+              contentContainerStyle={{ paddingLeft: 22 }}
+              renderItem={({ item }) => {
+                return (
+                  <TouchableOpacity onPress={() => navigation.navigate('News', { news: item })}>
+                    <ColumnCards
+                      image={item.urlToImage}
+                      author={item.author != null ? item.author : 'Unkown'}
+                      title={item.title}
+                      date={item.publishedAt}
+                    />
+                  </TouchableOpacity>
+                )
+              }}
+            />
+            <View style={styles.footer}>
+              <Button onPress={() => ''} />
+            </View>
+          </View>
         )}
       </View>
     </ScrollView>
@@ -118,6 +124,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#191C21',
     paddingTop: 22,
     flex: 1
+  },
+  footer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 16
   },
   shimmerImage: {
     width: 218,
