6346909e29c7b0518cc1b15a9d1ffc43bd7d8524
GabrielZaffalon <gabrielzaffalon19@gmail.com>
2022-08-09 19:05:46 -0300

    fix: remove comments

--- BooksStore/src/services/books.js
+++ BooksStore/src/services/books.js
@@ -3,8 +3,4 @@ import api from '../provider/api';
 export const showBooks = query =>
   api.get(`/v1/volumes?q=${query}&langRestrict=pt`);
 
-// fetch(
-//   `https://www.googleapis.com/books/v1/volumes?q=${query}&langRestrict=pt&key=AIzaSyBPre-eMyIPNY8HcA-KT6C9RF1FblEmj7U`,
-// ).then(response => response.json());
-
-//api.get('...', { params: { query, langRestrict: 'pt' } })
+//!api.get('...', { params: { query, langRestrict: 'pt' } })
